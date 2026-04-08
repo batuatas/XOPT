3a60eae2c7f2cb0a0a070334b45ffc9e3d435bc6
batuatas <726476ba@eur.nl>
2026-04-08 16:08:42 +0200

    chore: Purge leftover v2/v3 modules that were accidentally cloned into workspace_v4/src, and clean up floating root files

--- workspace_v4/src/xoptpoe_v2_modeling/__init__.py
+++
@@ -1,8 +0,0 @@
-from .features import (
-    FEATURE_SET_ORDER,
-    DEFAULT_FEATURE_SET,
-    FeatureSetBuild,
-    build_feature_set_manifest,
-    summarize_feature_sets,
-    feature_columns_for_set,
-)
